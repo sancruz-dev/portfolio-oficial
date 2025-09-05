83550d302d2f57cb5d15a3a97c00d2abfe278a6c
sancruz-dev <sancruz.dev@gmail.com>
2025-09-05 18:32:11 -0300

    Fixing 502 error of the Feedback feature

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,10 +1,18 @@
+import dotenv from 'dotenv';
 import express from 'express';
 import cors from 'cors'
 import { routes } from './routes';
 
+dotenv.config();
+
 const app = express();
 
-app.use(cors());
+app.use(cors({
+  origin: ["https://seu-front.vercel.app"], // ou "*" apenas para testes
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  allowedHeaders: ["Content-Type", "Authorization"]
+}));
+
 app.use(express.json());// [A]...
 app.use(routes);
 
